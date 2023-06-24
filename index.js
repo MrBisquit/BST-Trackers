9060ef0bd75a5d733d74607cc32838650b9abbb1
MrBisquit <william@wtdawson.info>
2023-06-24 14:21:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const ejs = require("ejs");
+const expressLayouts = require("express-ejs-layouts");
+const crypto = require("crypto");
+const moment = require("moment");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+
+app.use(cookieParser());
+
+app.set('views', './pages');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '10mb'}));
+app.use(express.static(__dirname + '/public', {}));
+
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
+
+app.get("/", (req, res) => {
+    if(!TID_exists(req.cookies._tid)) {
+        const tid = crypto.randomUUID();
+        Create_TID(tid);
+        res.cookie("_tid", tid);
+    }
+    res.render("index.ejs");
+});
+
+app.post("/tracking_data/", (req, res) => {
+    let data = {
+        data_from : moment(),
+        data : req.body
+    }
+    console.log(req.body);
+    AddTD_TID(req.cookies._tid, data);
+
+    res.json({ success : true, TID : req.cookies._tid});
+});
+
+function TID_exists(tid) {
+    let tids = JSON.parse(fs.readFileSync("./tracking_ids.json"));
+    console.log(tids[tid]);
+    if(tids[tid] == undefined || tids[tid] == null || tids[tid] == "") {
+        return false;
+    }
+    return true;
+}
+
+function Create_TID(tid) {
+    let tids = JSON.parse(fs.readFileSync("./tracking_ids.json"));
+    let tid_data = {
+        tid : tid,
+        td : [],
+        ti : {},
+        last_ping : moment(),
+        tracking_session_created : moment()
+    }
+    tids[tid] = tid_data;
+    //console.log(tids);
+    fs.writeFileSync("./tracking_ids.json", JSON.stringify(tids));
+}
+
+function AddTD_TID(tid, tracking_data) {
+    let tids = JSON.parse(fs.readFileSync("./tracking_ids.json"));
+    //console.log(tids);
+    //console.log(tid);
+    tids[tid].td.push(tracking_data);
+    tids[tid].last_ping = moment();
+    fs.writeFileSync("./tracking_ids.json", JSON.stringify(tids));
+}
+
+app.listen(80);
---
+++ public/tracking.js
@@ -0,0 +1,626 @@
+let array = [];
+
+function AddTrackingData(type, event) {
+    if(localStorage.getItem("_td") != null) {
+        array = JSON.parse(localStorage.getItem("_td"));
+    }
+
+    array.push({
+        type : type,
+        event : event,
+        //location : location,
+        //document : document,
+        //window : Window
+    })
+
+    //console.log(array);
+    /*console.log({
+        type : type,
+        event : event
+    });*/
+
+    localStorage.setItem("_td", JSON.stringify(array));
+
+    //document.getElementById("d").innerHTML = `${array.length} events captured:<br>${JSON.stringify(array)}<br>${array.length} events captured`;
+    //console.log(event);
+}
+
+function ClearAll() {
+    array = [];
+    localStorage.setItem("_td", JSON.stringify(array));
+}
+
+setInterval(async () => {
+    // Thanks to https://stackoverflow.com/a/29823632/16426057 for this!
+    (async () => {
+        const rawResponse = await fetch('/tracking_data/', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(array)
+        });
+        const content = await rawResponse.json();
+      
+        console.log(content);
+
+        array = [];
+      })();
+}, 5000);
+
+/*document.addEventListener("mouseenter", (ev) => {
+    AddTrackingData("mouseenter", ev);
+});
+
+document.addEventListener("scroll", (ev) => {
+    AddTrackingData("scroll", ev);
+});
+
+document.addEventListener("DOMContentLoaded", (ev) => {
+    AddTrackingData("DOMContentLoaded", ev);
+});
+
+document.addEventListener("abort", (ev) => {
+    AddTrackingData("abort", ev);
+});
+
+document.addEventListener("animationcancel", (ev) => {
+    AddTrackingData("animationcancel", ev);
+});
+
+document.addEventListener("animationend", (ev) => {
+    AddTrackingData("animationend", ev);
+});
+
+document.addEventListener("animationiteration", (ev) => {
+    AddTrackingData("animationiteration", ev);
+});
+
+document.addEventListener("animationstart", (ev) => {
+    AddTrackingData("animationstart", ev);
+});
+
+document.addEventListener("auxclick", (ev) => {
+    AddTrackingData("auxclick", ev);
+});
+
+document.addEventListener("beforeinput", (ev) => {
+    AddTrackingData("beforeinput", ev);
+});
+
+document.addEventListener("blur", (ev) => {
+    AddTrackingData("blur", ev);
+});
+
+document.addEventListener("cancel", (ev) => {
+    AddTrackingData("cancel", ev);
+});
+
+document.addEventListener("canplay", (ev) => {
+    AddTrackingData("canplay", ev);
+});
+
+document.addEventListener("canplaythrough", (ev) => {
+    AddTrackingData("canplaythrough", ev);
+});
+
+document.addEventListener("change", (ev) => {
+    AddTrackingData("change", ev);
+});
+
+document.addEventListener("click", (ev) => {
+    AddTrackingData("click", ev);
+});
+
+document.addEventListener("close", (ev) => {
+    AddTrackingData("close", ev);
+});
+
+document.addEventListener("compositionend", (ev) => {
+    AddTrackingData("compositionend", ev);
+});
+
+document.addEventListener("compositionstart", (ev) => {
+    AddTrackingData("compositionstart", ev);
+});
+
+document.addEventListener("compositionupdate", (ev) => {
+    AddTrackingData("compositionupdate", ev);
+});
+
+document.addEventListener("contextmenu", (ev) => {
+    AddTrackingData("contextmenu", ev);
+});
+
+document.addEventListener("copy", (ev) => {
+    AddTrackingData("copy", ev);
+});
+
+document.addEventListener("cuechange", (ev) => {
+    AddTrackingData("cuechange", ev);
+});
+
+document.addEventListener("cut", (ev) => {
+    AddTrackingData("cut", ev);
+});
+
+document.addEventListener("dblclick", (ev) => {
+    AddTrackingData("dblclick", ev);
+});
+
+document.addEventListener("drag", (ev) => {
+    AddTrackingData("drag", ev);
+});
+
+document.addEventListener("dragend", (ev) => {
+    AddTrackingData("dragend", ev);
+});
+
+document.addEventListener("dragenter", (ev) => {
+    AddTrackingData("dragenter", ev);
+});
+
+document.addEventListener("dragleave", (ev) => {
+    AddTrackingData("dragleave", ev);
+});
+
+document.addEventListener("dragover", (ev) => {
+    AddTrackingData("dragover", ev);
+});
+
+document.addEventListener("dragstart", (ev) => {
+    AddTrackingData("dragstart", ev);
+});
+
+document.addEventListener("drop", (ev) => {
+    AddTrackingData("drop", ev);
+});
+
+document.addEventListener("durationchange", (ev) => {
+    AddTrackingData("durationchange", ev);
+});
+
+document.addEventListener("emptied", (ev) => {
+    AddTrackingData("emptied", ev);
+});
+
+document.addEventListener("ended", (ev) => {
+    AddTrackingData("ended", ev);
+});
+
+document.addEventListener("error", (ev) => {
+    AddTrackingData("error", ev);
+});
+
+document.addEventListener("focusin", (ev) => {
+    AddTrackingData("focusin", ev);
+});
+
+document.addEventListener("focusout", (ev) => {
+    AddTrackingData("focusout", ev);
+});
+
+document.addEventListener("formdata", (ev) => {
+    AddTrackingData("formdata", ev);
+});
+
+document.addEventListener("fullscreenchange", (ev) => {
+    AddTrackingData("fullscreenchange", ev);
+});
+
+document.addEventListener("fullscreenerror", (ev) => {
+    AddTrackingData("fullscreenerror", ev);
+});
+
+document.addEventListener("gotpointercapture", (ev) => {
+    AddTrackingData("gotpointercapture", ev);
+});
+
+document.addEventListener("input", (ev) => {
+    AddTrackingData("input", ev);
+});
+
+document.addEventListener("invalid", (ev) => {
+    AddTrackingData("invalid", ev);
+});
+
+document.addEventListener("keydown", (ev) => {
+    AddTrackingData("keydown", ev);
+});
+
+document.addEventListener("keypress", (ev) => {
+    AddTrackingData("keypress", ev);
+});
+
+document.addEventListener("keyup", (ev) => {
+    AddTrackingData("keyup", ev);
+});
+
+document.addEventListener("load", (ev) => {
+    AddTrackingData("load", ev);
+});
+
+document.addEventListener("loadeddata", (ev) => {
+    AddTrackingData("loadeddata", ev);
+});
+
+document.addEventListener("loadedmetadata", (ev) => {
+    AddTrackingData("loadedmetadata", ev);
+});
+
+document.addEventListener("loadstart", (ev) => {
+    AddTrackingData("loadstart", ev);
+});
+
+document.addEventListener("lostpointercapture", (ev) => {
+    AddTrackingData("lostpointercapture", ev);
+});
+
+document.addEventListener("mousedown", (ev) => {
+    AddTrackingData("mousedown", ev);
+});*/
+
+document.addEventListener("mouseenter", (ev) => {
+    AddTrackingData("mouseenter", ev);
+});
+
+document.addEventListener("scroll", (ev) => {
+    AddTrackingData("scroll", ev);
+});
+
+document.addEventListener("DOMContentLoaded", (ev) => {
+    AddTrackingData("DOMContentLoaded", ev);
+});
+
+document.addEventListener("abort", (ev) => {
+    AddTrackingData("abort", ev);
+});
+
+document.addEventListener("animationcancel", (ev) => {
+    AddTrackingData("animationcancel", ev);
+});
+
+document.addEventListener("animationend", (ev) => {
+    AddTrackingData("animationend", ev);
+});
+
+document.addEventListener("animationiteration", (ev) => {
+    AddTrackingData("animationiteration", ev);
+});
+
+document.addEventListener("animationstart", (ev) => {
+    AddTrackingData("animationstart", ev);
+});
+
+document.addEventListener("auxclick", (ev) => {
+    AddTrackingData("auxclick", ev);
+});
+
+document.addEventListener("beforeinput", (ev) => {
+    AddTrackingData("beforeinput", ev);
+});
+
+document.addEventListener("blur", (ev) => {
+    AddTrackingData("blur", ev);
+});
+
+document.addEventListener("cancel", (ev) => {
+    AddTrackingData("cancel", ev);
+});
+
+document.addEventListener("canplay", (ev) => {
+    AddTrackingData("canplay", ev);
+});
+
+document.addEventListener("canplaythrough", (ev) => {
+    AddTrackingData("canplaythrough", ev);
+});
+
+document.addEventListener("change", (ev) => {
+    AddTrackingData("change", ev);
+});
+
+document.addEventListener("click", (ev) => {
+    AddTrackingData("click", ev);
+});
+
+document.addEventListener("close", (ev) => {
+    AddTrackingData("close", ev);
+});
+
+document.addEventListener("compositionend", (ev) => {
+    AddTrackingData("compositionend", ev);
+});
+
+document.addEventListener("compositionstart", (ev) => {
+    AddTrackingData("compositionstart", ev);
+});
+
+document.addEventListener("compositionupdate", (ev) => {
+    AddTrackingData("compositionupdate", ev);
+});
+
+document.addEventListener("contextmenu", (ev) => {
+    AddTrackingData("contextmenu", ev);
+});
+
+document.addEventListener("copy", (ev) => {
+    AddTrackingData("copy", ev);
+});
+
+document.addEventListener("cuechange", (ev) => {
+    AddTrackingData("cuechange", ev);
+});
+
+document.addEventListener("cut", (ev) => {
+    AddTrackingData("cut", ev);
+});
+
+document.addEventListener("dblclick", (ev) => {
+    AddTrackingData("dblclick", ev);
+});
+
+document.addEventListener("drag", (ev) => {
+    AddTrackingData("drag", ev);
+});
+
+document.addEventListener("dragend", (ev) => {
+    AddTrackingData("dragend", ev);
+});
+
+document.addEventListener("dragenter", (ev) => {
+    AddTrackingData("dragenter", ev);
+});
+
+document.addEventListener("dragleave", (ev) => {
+    AddTrackingData("dragleave", ev);
+});
+
+document.addEventListener("dragover", (ev) => {
+    AddTrackingData("dragover", ev);
+});
+
+document.addEventListener("dragstart", (ev) => {
+    AddTrackingData("dragstart", ev);
+});
+
+document.addEventListener("drop", (ev) => {
+    AddTrackingData("drop", ev);
+});
+
+document.addEventListener("durationchange", (ev) => {
+    AddTrackingData("durationchange", ev);
+});
+
+document.addEventListener("emptied", (ev) => {
+    AddTrackingData("emptied", ev);
+});
+
+document.addEventListener("ended", (ev) => {
+    AddTrackingData("ended", ev);
+});
+
+document.addEventListener("error", (ev) => {
+    AddTrackingData("error", ev);
+});
+
+document.addEventListener("focusin", (ev) => {
+    AddTrackingData("focusin", ev);
+});
+
+document.addEventListener("focusout", (ev) => {
+    AddTrackingData("focusout", ev);
+});
+
+document.addEventListener("formdata", (ev) => {
+    AddTrackingData("formdata", ev);
+});
+
+document.addEventListener("fullscreenchange", (ev) => {
+    AddTrackingData("fullscreenchange", ev);
+});
+
+document.addEventListener("fullscreenerror", (ev) => {
+    AddTrackingData("fullscreenerror", ev);
+});
+
+document.addEventListener("gotpointercapture", (ev) => {
+    AddTrackingData("gotpointercapture", ev);
+});
+
+document.addEventListener("input", (ev) => {
+    AddTrackingData("input", ev);
+});
+
+document.addEventListener("invalid", (ev) => {
+    AddTrackingData("invalid", ev);
+});
+
+document.addEventListener("keydown", (ev) => {
+    AddTrackingData("keydown", ev);
+});
+
+document.addEventListener("keypress", (ev) => {
+    AddTrackingData("keypress", ev);
+});
+
+document.addEventListener("keyup", (ev) => {
+    AddTrackingData("keyup", ev);
+});
+
+document.addEventListener("load", (ev) => {
+    AddTrackingData("load", ev);
+});
+
+document.addEventListener("loadeddata", (ev) => {
+    AddTrackingData("loadeddata", ev);
+});
+
+document.addEventListener("loadedmetadata", (ev) => {
+    AddTrackingData("loadedmetadata", ev);
+});
+
+document.addEventListener("loadstart", (ev) => {
+    AddTrackingData("loadstart", ev);
+});
+
+document.addEventListener("lostpointercapture", (ev) => {
+    AddTrackingData("lostpointercapture", ev);
+});
+
+document.addEventListener("mousedown", (ev) => {
+    AddTrackingData("mousedown", ev);
+});
+
+document.addEventListener("mousemove", (ev) => {
+    AddTrackingData("mousemove", ev);
+});
+
+document.addEventListener("mouseout", (ev) => {
+    AddTrackingData("mouseout", ev);
+});
+
+document.addEventListener("mouseover", (ev) => {
+    AddTrackingData("mouseover", ev);
+});
+
+document.addEventListener("mouseup", (ev) => {
+    AddTrackingData("mouseup", ev);
+});
+
+document.addEventListener("paste", (ev) => {
+    AddTrackingData("paste", ev);
+});
+
+document.addEventListener("pause", (ev) => {
+    AddTrackingData("pause", ev);
+});
+
+document.addEventListener("play", (ev) => {
+    AddTrackingData("play", ev);
+});
+
+document.addEventListener("playing", (ev) => {
+    AddTrackingData("playing", ev);
+});
+
+document.addEventListener("pointerlockchange", (ev) => {
+    AddTrackingData("pointerlockchange", ev);
+});
+
+document.addEventListener("pointerlockerror", (ev) => {
+    AddTrackingData("pointerlockerror", ev);
+});
+
+document.addEventListener("progress", (ev) => {
+    AddTrackingData("progress", ev);
+});
+
+document.addEventListener("ratechange", (ev) => {
+    AddTrackingData("ratechange", ev);
+});
+
+document.addEventListener("reset", (ev) => {
+    AddTrackingData("reset", ev);
+});
+
+document.addEventListener("resize", (ev) => {
+    AddTrackingData("resize", ev);
+});
+
+document.addEventListener("scroll", (ev) => {
+    AddTrackingData("scroll", ev);
+});
+
+document.addEventListener("search", (ev) => {
+    AddTrackingData("search", ev);
+});
+
+document.addEventListener("seeked", (ev) => {
+    AddTrackingData("seeked", ev);
+});
+
+document.addEventListener("seeking", (ev) => {
+    AddTrackingData("seeking", ev);
+});
+
+document.addEventListener("select", (ev) => {
+    AddTrackingData("select", ev);
+});
+
+document.addEventListener("selectionchange", (ev) => {
+    AddTrackingData("selectionchange", ev);
+});
+
+document.addEventListener("selectstart", (ev) => {
+    AddTrackingData("selectstart", ev);
+});
+
+document.addEventListener("show", (ev) => {
+    AddTrackingData("show", ev);
+});
+
+document.addEventListener("stalled", (ev) => {
+    AddTrackingData("stalled", ev);
+});
+
+document.addEventListener("submit", (ev) => {
+    AddTrackingData("submit", ev);
+});
+
+document.addEventListener("suspend", (ev) => {
+    AddTrackingData("suspend", ev);
+});
+
+document.addEventListener("timeupdate", (ev) => {
+    AddTrackingData("timeupdate", ev);
+});
+
+document.addEventListener("toggle", (ev) => {
+    AddTrackingData("toggle", ev);
+});
+
+document.addEventListener("touchcancel", (ev) => {
+    AddTrackingData("touchcancel", ev);
+});
+
+document.addEventListener("touchend", (ev) => {
+    AddTrackingData("touchend", ev);
+});
+
+document.addEventListener("touchmove", (ev) => {
+    AddTrackingData("touchmove", ev);
+});
+
+document.addEventListener("touchstart", (ev) => {
+    AddTrackingData("touchstart", ev);
+});
+
+document.addEventListener("transitioncancel", (ev) => {
+    AddTrackingData("transitioncancel", ev);
+});
+
+document.addEventListener("transitionend", (ev) => {
+    AddTrackingData("transitionend", ev);
+});
+
+document.addEventListener("transitionrun", (ev) => {
+    AddTrackingData("transitionrun", ev);
+});
+
+document.addEventListener("transitionstart", (ev) => {
+    AddTrackingData("transitionstart", ev);
+});
+
+document.addEventListener("volumechange", (ev) => {
+    AddTrackingData("volumechange", ev);
+});
+
+document.addEventListener("waiting", (ev) => {
+    AddTrackingData("waiting", ev);
+});
+
+document.addEventListener("wheel", (ev) => {
+    AddTrackingData("wheel", ev);
+});
